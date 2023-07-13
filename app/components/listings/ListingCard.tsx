29337c79288b4ea0257001dbe6d2919aa3e50a70
jiri0521 <47916493+jiri0521@users.noreply.github.com>
2023-07-13 13:32:50 +0900

    Update ListingCard.tsx

--- app/components/listings/ListingCard.tsx
+++ app/components/listings/ListingCard.tsx
@@ -132,14 +132,7 @@ const ListingCard: React.FC<ListingCardProps> = ({
         <div className="font-light text-neutral-500">
           投稿日:{formattedDate}
         </div>
-        <div className="flex flex-row items-center gap-1">
-          <div className="font-semibold">
-            ¥ {price}
-          </div>
-          {!reservation && (
-            <div className="font-light">1泊</div>
-          )}
-        </div>
+      
         {onAction && actionLabel && (
           <Button
             disabled={disabled}
