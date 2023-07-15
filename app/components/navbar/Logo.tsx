5be2cf49aa4409c5609063685082e947855c8514
jiri0521 <47916493+jiri0521@users.noreply.github.com>
2023-07-15 11:16:52 +0900

    Update Logo.tsx

--- app/components/navbar/Logo.tsx
+++ app/components/navbar/Logo.tsx
@@ -13,7 +13,7 @@ const Logo = () => {
             className="hidden md:block cursor-pointer"
             height="100"
             width="100"
-            src="/images/logo1.png"
+            src="/images/さんりつ会ロゴ透明.png"
             />
     )
 
