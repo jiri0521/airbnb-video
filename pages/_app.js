595ea1a57820b92b5e9718879913abce77be0faf
jiri0521 <47916493+jiri0521@users.noreply.github.com>
2023-07-17 15:12:55 +0900

    Add files via upload

---
+++ pages/_app.js
@@ -0,0 +1,24 @@
+// pages/_app.js
+
+import { useEffect } from 'react';
+
+function MyApp({ Component, pageProps }) {
+  useEffect(() => {
+    if ('serviceWorker' in navigator) {
+      window.addEventListener('load', () => {
+        navigator.serviceWorker
+          .register('/public/service-worker.js')
+          .then((registration) => {
+            console.log('Service Worker registered: ', registration);
+          })
+          .catch((error) => {
+            console.log('Service Worker registration failed: ', error);
+          });
+      });
+    }
+  }, []);
+
+  return <Component {...pageProps} />;
+}
+
+export default MyApp;
