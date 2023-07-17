9fb8cbdff2617b19ec6b58943ff1c8ecaa889d72
jiri0521 <47916493+jiri0521@users.noreply.github.com>
2023-07-17 11:28:28 +0900

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,4 +1,20 @@
 /** @type {import('next').NextConfig} */
+
+const withPWA = require('next-pwa');
+
+module.exports = withPWA({
+  pwa: {
+    name: "zaitaku video",
+    shortName:"zaitaku",
+    register: true,
+    dest: 'public',
+    skipWating: true,
+    disable: process.env.NODE_ENV === 'development',
+    scope: '/',
+    sw: 'service-worker.js',
+  },
+});
+
 const nextConfig = {
     experimental:{
         appDir: true,
