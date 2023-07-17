c52f244546bde6893c3d193bd2c7c00a790763fd
jiri0521 <47916493+jiri0521@users.noreply.github.com>
2023-07-18 00:31:56 +0900

    Update _document.js

--- pages/_document.js
+++ pages/_document.js
@@ -6,6 +6,7 @@ class MyDocument extends Document {
       <Html>
         <Head>
           <link rel="manifest" href="/manifest.json" />
+        <link rel="service-worker" href="/service-worker.js" />
           <link rel="apple-touch-icon" href="/icon-192x192.png"></link>
           <meta name="theme-color" content="#fff" />
         </Head>
