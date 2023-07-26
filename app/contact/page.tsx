e74c77e68468050c42c9573d6e8b0c9b4dedaedd
jiri0521 <47916493+jiri0521@users.noreply.github.com>
2023-07-26 09:13:02 +0900

    Add files via upload

---
+++ app/contact/page.tsx
@@ -0,0 +1,18 @@
+
+import ClientOnly from "@/app/components/ClientOnly";
+
+
+
+
+const ContactPage =  () => {
+  
+    return (
+      <ClientOnly>
+        <div className="w-full bg-white z-10 shadow-sm">
+          <iframe src="https://docs.google.com/forms/d/e/1FAIpQLScKlYLRQKhzubFIQ3sTMKJKot8S3xZy25cErEwtA9JDE_vYdw/viewform?embedded=true" width="640" height="1002" >読み込んでいます…</iframe>
+        </div>
+      </ClientOnly>
+    );
+  }
+   
+  export default ContactPage;
