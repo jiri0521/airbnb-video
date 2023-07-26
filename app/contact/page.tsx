c26e37a08c0003ce324631450c52a8ffce93ee36
jiri0521 <47916493+jiri0521@users.noreply.github.com>
2023-07-26 10:52:53 +0900

    Update page.tsx

--- app/contact/page.tsx
+++ app/contact/page.tsx
@@ -9,7 +9,7 @@ const ContactPage =  () => {
     return (
       <ClientOnly>
         <div className="w-full bg-white z-10 shadow-sm">
-          <iframe src="https://docs.google.com/forms/d/e/1FAIpQLScKlYLRQKhzubFIQ3sTMKJKot8S3xZy25cErEwtA9JDE_vYdw/viewform?embedded=true" width="640" height="1002" >読み込んでいます…</iframe>
+          <iframe src="https://docs.google.com/forms/d/e/1FAIpQLScKlYLRQKhzubFIQ3sTMKJKot8S3xZy25cErEwtA9JDE_vYdw/viewform?embedded=true" width="540" height="1002" >読み込んでいます…</iframe>
         </div>
       </ClientOnly>
     );
