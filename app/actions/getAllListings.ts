c84daf7ce7b8a086053d5608accbe430a6141300
jiri0521 <47916493+jiri0521@users.noreply.github.com>
2023-07-31 12:48:31 +0900

    Add files via upload

---
+++ app/actions/getAllListings.ts
@@ -0,0 +1,35 @@
+import prisma from "@/app/libs/prismadb";
+
+
+export default async function getAllListings(params: any) {
+        try {
+
+            const {result} = params
+    
+            let query: any = {};
+    
+            if(result) {
+                query.title = {
+                    contains:result
+                }
+            }
+    
+            const listings = await prisma.listing.findMany({
+                where:query,
+                orderBy: {
+                    createdAt:'desc'
+                }
+            });
+    
+            const safeListings = listings.map((listing) => ({
+                ...listing,
+                createdAt:listing.createdAt.toISOString()
+            }))
+    
+            return safeListings
+    
+        }catch(error:any) {
+            throw new Error(error)
+        }
+
+    }
