3843d5344f8e2c36f5afb5f0b7eefaed243e7483
jiri0521 <47916493+jiri0521@users.noreply.github.com>
2023-08-18 09:51:05 +0900

    Update _document.js

--- pages/_document.js
+++ pages/_document.js
@@ -1,4 +1,5 @@
 import Document, { Html, Head, Main, NextScript } from "next/document";
+import OneSignal from 'react-onesignal';
 
 class MyDocument extends Document {
   render() {
@@ -6,9 +7,22 @@ class MyDocument extends Document {
       <Html>
         <Head>
           <link rel="manifest" href="/manifest.json" />
-        <link rel="service-worker" href="/service-worker.js" />
+          <link rel="service-worker" href="/service-worker.js" />
           <link rel="apple-touch-icon" href="/icon-192x192.png"></link>
           <meta name="theme-color" content="#fff" />
+          <script src="https://cdn.onesignal.com/sdks/web/v16/OneSignalSDK.page.js" defer></script>
+<script>
+  window.OneSignalDeferred = window.OneSignalDeferred || [];
+  OneSignalDeferred.push(function(OneSignal) {
+    OneSignal.init({
+      appId: "584d9f8f-3fb4-4843-ad96-5c400641f934",
+      safari_web_id: "web.onesignal.auto.10485988-1822-4e96-b399-29edb7cde282",
+      notifyButton: {
+        enable: true,
+      },
+    })
+  });
+</script>
         </Head>
         <body>
           <Main />
