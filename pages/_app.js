eba70304422acbd974ef5dc15b0c149d3dca6975
jiri0521 <47916493+jiri0521@users.noreply.github.com>
2023-08-18 10:21:39 +0900

    Update _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -17,7 +17,23 @@ function MyApp({ Component, pageProps }) {
       });
     }
   }, []);
+  useEffect(() => {
+    window.OneSignal = window.OneSignal || [];
+    OneSignal.push(function () {
+        OneSignal.init({
+            appId: "584d9f8f-3fb4-4843-ad96-5c400641f934",
+            notifyButton: {
+                enable: true,
+            },
 
+            allowLocalhostAsSecureOrigin: true,
+        });
+    });
+
+    return () => {
+        window.OneSignal = undefined;
+    };
+}, []); // <-- run this effect once on mount
   return <Component {...pageProps} />;
 }
 
