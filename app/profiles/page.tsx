fb22c7f2b1694af1427eea69c7c9f8683e0b8ce3
jiri0521 <47916493+jiri0521@users.noreply.github.com>
2023-08-20 16:43:34 +0900

    Update page.tsx

--- app/profiles/page.tsx
+++ app/profiles/page.tsx
@@ -1,16 +1,11 @@
 'use client';
 
-import axios from 'axios';
 import React, { useState } from 'react'
 import { useRouter } from 'next/navigation';
-import { FieldValues, SubmitHandler, useForm } from 'react-hook-form';
 import { User } from '@prisma/client'
-import { CldUploadButton } from 'next-cloudinary';
 
-import Input from "../components/inputs/Input";
-import Button from '../components/ChatButton';
-import Image from 'next/image';
-import { toast } from 'react-hot-toast';
+import Avatar from '../components/ChatAvatar';
+import SettingsModal from '../components/sidebar/SettingsModal';
 
 
 interface ProfileProps {
@@ -20,128 +15,24 @@ interface ProfileProps {
   const ProfilePage: React.FC<ProfileProps> = ({
   currentUser
   }) => {
-    const router = useRouter();
-    const [isLoading, setIsLoading] = useState(false);
 
-    console.log(currentUser, '&CHANGE_USER_PROFILE')
+    const [isOpen, setIsOpen] = useState(false);
+    console.log({ currentUser, }, 'Profile')
 
-    const {
-        register,
-        handleSubmit,
-        setValue,
-        watch,
-        formState: {
-          errors,
-        }
-      } = useForm<FieldValues>({
-          defaultValues: {
-            name: ""    
-          }
-      });
-    
-      const image = watch('image');
-    
-      const handleUpload = (result: any) => {
-        setValue('image', result.info.secure_url, { 
-          shouldValidate: true 
-        });
-      }
-    
-      const onSubmit: SubmitHandler<FieldValues> = (data) => {
-        setIsLoading(true);
-    
-        axios.post('/api/settings', data)
-        .then(() => {
-          router.refresh();
-          
-        })
-        .catch(() => toast.error('Something went wrong!'))
-        .finally(() => setIsLoading(false));
-      }
-    
 
-  return (
-    <div>
-       <form onSubmit={handleSubmit(onSubmit)}>
-        <div className="space-y-20">
-          <div className="border-b border-gray-900/10 pb-12">
-            <h2 
-              className="
-                text-base 
-                font-semibold 
-                leading-7 
-                text-gray-900
-              "
-            >
-              プロフィール
-            </h2>
-            <p className="mt-1 text-sm leading-6 text-gray-600">
-              公開するプロフィールを編集できます。
-            </p>
-
-            <div className="mt-10 flex flex-col gap-y-8 min-w-[230px] lg:w-[620px] ">
-              <Input
-                disabled={isLoading}
-                label="Name" 
-                id="name" 
-                errors={errors} 
-                required 
-                register={register}
-              />
-              <div>
-                <label 
-                  htmlFor="photo" 
-                  className="
-                    block 
-                    text-sm 
-                    font-medium 
-                    leading-6 
-                    text-gray-900
-                  "
-                >
-                  アバターの変更
-                </label>
-                <div className="mt-2 flex items-center gap-x-3">
-                <Image
-                    width="48"
-                    height="48" 
-                    className="rounded-full" 
-                    src='/images/placeholder.jpg'
-                    alt="Avatar"
-                  />
-                 
-                  <CldUploadButton                   
-                    options={{ maxFiles: 1 }} 
-                    onUpload={handleUpload} 
-                    uploadPreset="slvovxli"
-                  ></CldUploadButton>
+return(
+  <>
+    <SettingsModal currentUser={currentUser} isOpen={isOpen} onClose={() => setIsOpen(false)} /><div
+      onClick={() => setIsOpen(true)}
+      className="cursor-pointer hover:opacity-75 transition"
+    >
+      <Avatar user={currentUser} />
+<div>クリック！</div>
+    </div>
+  </>
+);
                     
-                  
-                </div>
-              </div>
-            </div>
-          </div>
-        </div>
-
-        <div 
-          className="
-            mt-6 
-            flex 
-            items-center 
-            justify 
-            gap-x-6
-          "
-        >
-          <Button 
-            disabled={isLoading}
-            type="submit"
-          >
-            保存
-          </Button>
-        </div>
-      </form>
-     </div>
-  )
+   
 }
 
 export default ProfilePage;
