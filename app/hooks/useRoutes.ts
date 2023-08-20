a78b2d86e102de6e365a7289d93ba978b5fba1d9
jiri0521 <47916493+jiri0521@users.noreply.github.com>
2023-08-20 14:12:59 +0900

    Update useRoutes.ts

--- app/hooks/useRoutes.ts
+++ app/hooks/useRoutes.ts
@@ -14,18 +14,18 @@ const useRoutes = () => {
   const pathname = usePathname();
   const { conversationId } = useConversation();
 
-  const routes = useMemo(() => [
-    { 
-      label: 'Chat', 
-      href: '/conversations', 
-      icon: HiChat,
-      active: pathname === '/conversations' || !!conversationId
-    },
+  const routes = useMemo(() => [    
     { 
       label: 'Users', 
       href: '/users', 
       icon: HiUsers, 
       active: pathname === '/users'
+    },
+    { 
+      label: 'Chat', 
+      href: '/conversations', 
+      icon: HiChat,
+      active: pathname === '/conversations' || !!conversationId
     },
      {
       label: 'favorites', 
