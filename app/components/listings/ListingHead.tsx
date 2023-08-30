74a722f9308b90d22c3d8019d0105f030af4d3d0
jiri0521 <47916493+jiri0521@users.noreply.github.com>
2023-08-30 13:37:38 +0900

    Update ListingHead.tsx

--- app/components/listings/ListingHead.tsx
+++ app/components/listings/ListingHead.tsx
@@ -75,12 +75,7 @@ const ListingHead: React.FC<ListingHeadProps> = ({
         </div>
         
       </div>
-      <div
-        className="fixed z-50 bottom-5 right-10 py-2 px-2 bg-gray-100
-                  border-2 hover:bg-gray-200 hover:shadow-sm hover:translate-y-0.5 transform transition rounded-full cursor-pointer"
-        onClick={handleClick}
-        >
-      </div>
+     
     </>
       
    );
