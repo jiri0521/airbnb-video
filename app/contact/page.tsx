a5faf88fe5252244eadcc9237e30bb428568dbe2
jiri0521 <47916493+jiri0521@users.noreply.github.com>
2023-11-09 16:26:10 +0900

    Update page.tsx

--- app/contact/page.tsx
+++ app/contact/page.tsx
@@ -9,7 +9,7 @@ const ContactPage =  () => {
     return (
        <ClientOnly>
         <div>
-          <h1 className="text-center h-full mb-3">お問い合わせ</h1>
+          <h1 className="text-center  text-orange-400 h-full mb-3">お問い合わせ</h1>
            <form>
              <div className="mb-3">
                 <label htmlFor="name" className="mx-auto form-control px-4 py-2 min-w-[230px] lg:w-[620px] sm:px-5 sm:py-3 flex focus:ring-[1px] placeholder:text-zinc-400 form-label">お名前</label><div/>
