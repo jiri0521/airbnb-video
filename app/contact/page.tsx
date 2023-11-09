d7e2bc79675fc25e9fe808caa41c820ca5575a48
jiri0521 <47916493+jiri0521@users.noreply.github.com>
2023-11-09 16:24:01 +0900

    Update page.tsx

--- app/contact/page.tsx
+++ app/contact/page.tsx
@@ -7,11 +7,27 @@ import ClientOnly from "@/app/components/ClientOnly";
 const ContactPage =  () => {
   
     return (
-      <ClientOnly>
-        <div className="w-full bg-white z-10 shadow-sm">
-          <iframe src="https://docs.google.com/forms/d/e/1FAIpQLScKlYLRQKhzubFIQ3sTMKJKot8S3xZy25cErEwtA9JDE_vYdw/viewform?embedded=true" width="540" height="1002" >読み込んでいます…</iframe>
-        </div>
-      </ClientOnly>
+       <ClientOnly>
+        <div>
+          <h1 className="text-center h-full mb-3">お問い合わせ</h1>
+           <form>
+             <div className="mb-3">
+                <label htmlFor="name" className="mx-auto form-control px-4 py-2 min-w-[230px] lg:w-[620px] sm:px-5 sm:py-3 flex focus:ring-[1px] placeholder:text-zinc-400 form-label">お名前</label><div/>
+                <input type="text" className="mx-auto form-control px-4 py-2 min-w-[230px] lg:w-[620px] sm:px-5 sm:py-3 flex border focus:ring-[1px] placeholder:text-zinc-400" id="name" required />
+                <label htmlFor="email" className="mx-auto form-control px-4 py-2 min-w-[230px] lg:w-[620px] sm:px-5 sm:py-3 flex  focus:ring-[1px] placeholder:text-zinc-400 form-label">メールアドレス</label><div/>
+                <input type="email" className="mx-auto form-control px-4 py-2 min-w-[230px] lg:w-[620px] sm:px-5 sm:py-3 flex border focus:ring-[1px] placeholder:text-zinc-400" id="email" required />
+                <label htmlFor="message" className="mx-auto form-control px-4 py-2 min-w-[230px] lg:w-[620px] sm:px-5 sm:py-3 flex focus:ring-[1px] placeholder:text-zinc-400 form-label">メッセージ</label><div/>
+                <textarea 
+                  name="message"
+                  id="message"
+                  className="mx-auto form-control px-4 py-2 min-w-[230px] lg:w-[620px] sm:px-5 sm:py-3 flex border focus:ring-[1px] placeholder:text-zinc-400"
+                ></textarea>
+              </div>
+           </form>
+       
+       </div>
+         
+      </ClientOnly
     );
   }
    
