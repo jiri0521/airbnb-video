797182587c2e702536ee0fed20bb24f966624c8c
jiri0521 <47916493+jiri0521@users.noreply.github.com>
2023-11-18 11:16:02 +0900

    Update page.tsx

--- app/contact/page.tsx
+++ app/contact/page.tsx
@@ -1,41 +1,60 @@
-
-import ClientOnly from "@/app/components/ClientOnly";
-
+"use client";
 
 
-
-const ContactPage =  () => {
+const  Contact = () => {
+  return(
   
-    return (
-       
-        <div>
-          <h1 className="text-center  text-orange-400 h-full mb-3">お問い合わせ</h1>
-           <form>
-             <div className="mb-3">
-                <label htmlFor="name" className="mx-auto form-control px-4 py-2 min-w-[230px] lg:w-[620px] sm:px-5 sm:py-3 flex focus:ring-[1px] placeholder:text-zinc-400 form-label">お名前</label><div/>
-                <input type="text" className="mx-auto form-control px-4 py-2 min-w-[230px] lg:w-[620px] sm:px-5 sm:py-3 flex border focus:ring-[1px] placeholder:text-zinc-400" id="name" required />
-                <label htmlFor="email" className="mx-auto form-control px-4 py-2 min-w-[230px] lg:w-[620px] sm:px-5 sm:py-3 flex  focus:ring-[1px] placeholder:text-zinc-400 form-label">メールアドレス</label><div/>
-                <input type="email" className="mx-auto form-control px-4 py-2 min-w-[230px] lg:w-[620px] sm:px-5 sm:py-3 flex border focus:ring-[1px] placeholder:text-zinc-400" id="email" required />
-                <label htmlFor="message" className="mx-auto form-control px-4 py-2 min-w-[230px] lg:w-[620px] sm:px-5 sm:py-3 flex focus:ring-[1px] placeholder:text-zinc-400 form-label">メッセージ</label><div/>
-                <textarea 
-                  name="message"
-                  id="message"
-                  className="mx-auto form-control px-4 py-2 min-w-[230px] lg:w-[620px] sm:px-5 sm:py-3 flex border focus:ring-[1px] placeholder:text-zinc-400"
-                ></textarea>
-               
-              </div>
-              <button
-              type="submit"
-              className="flex border focus:ring-[1px] px-4 py-2 bg-blue-400 text-white mx-auto rounded"
-              >
-                メール送信
-              </button>
-           </form>
-       
-       </div>
-         
-      
-    );
-  }
-   
-  export default ContactPage;
+    <form id="my-form-id" method="post" action="https://api.formcake.com/api/form/f62cb2d3-8e3b-4d73-bc57-68458b32f39d/submission" className="mb-3md:w-1/2 w-5/6 p-5 border mx-auto my-5 bg-slate-200 rounded-lg">
+      <div className="flex flex-col items-start mb-3 justify-start">
+          <h1 className="text-xl font-semibold">お問い合わせ</h1>
+        </div>
+        <div className="mb-3 relative flex flex-col space-y-1">
+          <label htmlFor="name" className="mb-3 text-sm font-light text-gray-500">
+            名前
+          </label>
+          <input
+            name="name"
+            type="text"
+            id="name"
+            required
+            placeholder="そうわ たろう"
+            className="mb-3 rounded-xl border-2 border-gray-400 p-2"
+          />
+        </div>
+        <div className="mb-3 relative flex flex-col space-y-1">
+          <label htmlFor="name" className="mb-3 text-sm font-light text-gray-500">
+            メールアドレス
+          </label>
+          <input
+            name="email"
+            type="email"
+            id="email"
+            required
+            placeholder="sample@mail.com"
+            className="mb-3 rounded-xl border-2 border-gray-400 p-2"
+          />
+        </div>
+        <div className="mb-3 relative flex flex-col space-y-1">
+          <label htmlFor="massage" className="mb-3 text-sm font-light text-gray-500">
+            内容
+          </label>
+          <textarea
+            name="message"
+            id="message"
+            required
+            cols={10}
+            rows={5}    
+            placeholder="お問い合わせの内容をこちらに書いてください。"
+            className="mb-3 rounded-xl border-2 border-gray-400 p-2"
+          />
+        </div>
+        <div className="items-center justify-center">
+        <button type='submit' className="flex p-2 px-3 py-3 items-center justify-center rounded-lg bg-blue-600 text-white shadow-blue-500 hover:bg-blue-700 hover:shadow-md">
+          <span>送信</span>
+        </button>
+        </div>
+    </form>
+  
+  );
+}
+export default Contact;
