2d23cd322157654f325a0e6ac48f3910065ee07e
jiri0521 <47916493+jiri0521@users.noreply.github.com>
2024-01-16 16:11:42 +0900

    Update page.tsx

--- app/house/page.tsx
+++ app/house/page.tsx
@@ -7,7 +7,7 @@ import LayoutPlan from '../components/HouseLayout';
 const Home: NextPage = () => {
   return (
     <div className="container mx-auto">
-      <h1 className="text-center text-2xl font-bold my-4">3LDKの間取り</h1>
+      <h1 className="text-center text-2xl font-bold my-4">間取りから動画選択</h1>
       <LayoutPlan />
     </div>
   );
