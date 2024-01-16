61eb0e2ec975e5b44c973d769c4456dac51c5d09
jiri0521 <47916493+jiri0521@users.noreply.github.com>
2024-01-17 00:02:47 +0900

    Update HouseLayout.tsx

--- app/components/HouseLayout.tsx
+++ app/components/HouseLayout.tsx
@@ -1,32 +1,38 @@
 // components/LayoutPlan.tsx
-
 import Link from 'next/link';
 import Image from 'next/image';
-import layout from 'public/images/layout.svg'
+import layout from 'public/images/layout.svg';
+import balloon from 'public/images/balloon.svg'; // インポート確認済み
 
 const LayoutPlan = () => {
+  
+
   return (
     <div className="flex flex-col relative items-center">
+     
       <Image src={layout} alt="3LDK Layout" width={700} height={700} />
       <div className="absolute top-0 left-0 w-full h-full">
-        {/* キッチンへのリンク */}
+        {/* div要素に 'balloon' スタイルクラスとアニメーションを適用します */}
         <Link href="/?category=栄養">
-          <div className="block text-2xl text-orange-500 font-bold hover:bg-blue-100 animate-wiggle" style={{ 
-            position: 'absolute', top: '60%', left: '32%', width: '15%', height: '20%' }}>食事</div>
+        <Image src={balloon} className='w-full h-full animate-wiggle shadow-lg' alt="bollon-eiyou" style={{ position: 'absolute', top: '55%', left: '28%', width: '15%', height: '15%' }}/>
+          <div className='text font-bold text-orange-500 animate-wiggle' style={{ position: 'absolute', top: '57%', left: '32%'}}>栄養</div>
         </Link>
         {/* 寝室へのリンク */}
         <Link href="/?category=ベッド">
-          <div className="block text-2xl text-orange-500 font-bold hover:bg-blue-100 animate-wiggle" style={{
-            position: 'absolute', top: '28%', left: '58%', width: '10%', height: '15%' }}>寝室</div>
+        <Image src={balloon} className='w-full h-full animate-wiggle shadow-lg' alt="bollon-bed" style={{ position: 'absolute', top: '28%', left: '58%', width: '15%', height: '15%' }}/>
+          <div className="block text text-orange-500 font-bold hover:bg-blue-100 animate-wiggle" style={{
+            position: 'absolute', top: '30%', left: '62%', width: '10%', height: '15%' }}>寝室</div>
         </Link>
-        {/* 居間へのリンク */}
         <Link href="/?category=歩行">
-          <div className="block text-2xl text-orange-500 font-bold hover:bg-blue-100 animate-wiggle" style={{
-            position: 'absolute', top: '55%', left: '58%', width: '10%', height: '15%' }}>居間</div>
+        <Image src={balloon} className='w-full h-full animate-wiggle shadow-lg' alt="bollon-living" style={{ position: 'absolute', top: '55%', left: '58%', width: '15%', height: '15%' }}/>
+        <div className="block text text-orange-500 font-bold hover:bg-blue-100 animate-wiggle" style={{
+            position: 'absolute', top: '57%', left: '62%', width: '10%', height: '15%' }}>居間</div>
         </Link>
       </div>
-            <a className='text-gray-400' href="https://jp.freepik.com/free-vector/flat-architectural-plan-top-view-position_4015450.htm#query=%E9%96%93%E5%8F%96%E3%82%8A&position=43&from_view=keyword&track=sph&uuid=2a7df97a-0b8b-4987-a7d3-09c6f0cd81ba">イラストの著作者:macrovector<br></br>/出典:Freepik</a>
-
+     
+      <a className='text-gray-400'>
+        イラストの著作者:macrovector / 出典:Freepik
+      </a>
     </div>
   );
 };
