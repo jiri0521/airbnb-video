15ac790d95d6b677cdcac5ebb17ffc09a201ef86
jiri0521 <47916493+jiri0521@users.noreply.github.com>
2024-01-16 18:19:43 +0900

    Update HouseLayout.tsx

--- app/components/HouseLayout.tsx
+++ app/components/HouseLayout.tsx
@@ -11,15 +11,19 @@ const LayoutPlan = () => {
       <div className="absolute top-0 left-0 w-full h-full">
         {/* キッチンへのリンク */}
         <Link href="/?category=栄養">
-          <div className="block text-4xl text-orange-500 font-bold hover:bg-blue-100 animate-wiggle" style={{ 
+          <div className="block text-2xl text-orange-500 font-bold hover:bg-blue-100 animate-wiggle" style={{ 
             position: 'absolute', top: '60%', left: '32%', width: '15%', height: '20%' }}>食事</div>
         </Link>
-        {/* 居間へのリンク */}
+        {/* 寝室へのリンク */}
         <Link href="/?category=ベッド">
-          <div className="block text-4xl text-orange-500 font-bold hover:bg-blue-100 animate-wiggle" style={{
+          <div className="block text-2xl text-orange-500 font-bold hover:bg-blue-100 animate-wiggle" style={{
             position: 'absolute', top: '28%', left: '58%', width: '10%', height: '15%' }}>寝室</div>
         </Link>
-        {/* ...他の部屋も同様にリンクを追加 */}
+        {/* 居間へのリンク */}
+        <Link href="/?category=歩行">
+          <div className="block text-2xl text-orange-500 font-bold hover:bg-blue-100 animate-wiggle" style={{
+            position: 'absolute', top: '55%', left: '58%', width: '10%', height: '15%' }}>居間</div>
+        </Link>
       </div>
       <a href="https://jp.freepik.com/free-vector/flat-architectural-plan-top-view-position_4015450.htm#query=%E9%96%93%E5%8F%96%E3%82%8A&position=43&from_view=keyword&track=sph&uuid=2a7df97a-0b8b-4987-a7d3-09c6f0cd81ba">イラストの著作者：macrovector</a>／出典：Freepik
     </div>
