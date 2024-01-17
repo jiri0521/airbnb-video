98b5a96138988636a2c343de91e5d7baaf8500b5
jiri0521 <47916493+jiri0521@users.noreply.github.com>
2024-01-17 11:18:46 +0900

    Update HouseLayout.tsx

--- app/components/HouseLayout.tsx
+++ app/components/HouseLayout.tsx
@@ -13,20 +13,15 @@ const LayoutPlan = () => {
       <Image src={layout} alt="3LDK Layout" width={700} height={700} />
       <div className="absolute top-0 left-0 w-full h-full">
         {/* div要素に 'balloon' スタイルクラスとアニメーションを適用します */}
-        <Link href="/?category=栄養">
-        <Image src={balloon} className='w-full h-full animate-wiggle shadow-lg' alt="bollon-eiyou" style={{ position: 'absolute', top: '55%', left: '28%', width: '15%', height: '15%' }}/>
-          <div className='text font-bold text-orange-500 animate-wiggle' style={{ position: 'absolute', top: '57%', left: '32%'}}>食事</div>
+         <Link href="/?category=栄養">
+        <Image src={balloonKitchen} className='w-full h-full animate-wiggle shadow-lg' alt="bollon-eiyou" style={{ position: 'absolute', top: '55%', left: '28%', width: '15%', height: '15%' }}/>
         </Link>
         {/* 寝室へのリンク */}
         <Link href="/?category=ベッド">
-        <Image src={balloon} className='w-full h-full animate-wiggle shadow-lg' alt="bollon-bed" style={{ position: 'absolute', top: '28%', left: '58%', width: '15%', height: '15%' }}/>
-          <div className="block text text-orange-500 font-bold hover:bg-blue-100 animate-wiggle" style={{
-            position: 'absolute', top: '30%', left: '62%', width: '10%', height: '15%' }}>寝室</div>
+        <Image src={balloonBed} className='w-full h-full animate-wiggle shadow-lg' alt="bollon-bed" style={{ position: 'absolute', top: '28%', left: '58%', width: '15%', height: '15%' }}/>
         </Link>
         <Link href="/?category=歩行">
-        <Image src={balloon} className='w-full h-full animate-wiggle shadow-lg' alt="bollon-living" style={{ position: 'absolute', top: '55%', left: '58%', width: '15%', height: '15%' }}/>
-        <div className="block text text-orange-500 font-bold hover:bg-blue-100 animate-wiggle" style={{
-            position: 'absolute', top: '57%', left: '62%', width: '10%', height: '15%' }}>居間</div>
+        <Image src={balloonLiving} className='w-full h-full animate-wiggle shadow-lg' alt="bollon-living" style={{ position: 'absolute', top: '55%', left: '58%', width: '15%', height: '15%' }}/>
         </Link>
       </div>
      
