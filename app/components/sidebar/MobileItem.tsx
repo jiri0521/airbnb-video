9fbfaf00e4771d28aba2e47e6120a65be7c9d7f4
jiri0521 <47916493+jiri0521@users.noreply.github.com>
2024-01-18 18:35:57 +0900

    Update MobileItem.tsx

--- app/components/sidebar/MobileItem.tsx
+++ app/components/sidebar/MobileItem.tsx
@@ -1,49 +1,43 @@
+// MobileItem.tsx
 import Link from "next/link";
-
 import clsx from "clsx";
+import { IconType } from 'react-icons'; //React Iconsの型をインポート
 
 interface MobileItemProps {
   href: string;
-  icon: any;
+  icon: IconType;
   active?: boolean;
+  label: string; // ラベルのプロパティを追加
   onClick?: () => void;
 }
 
 const MobileItem: React.FC<MobileItemProps> = ({ 
-  href, 
-  icon: Icon, 
+  href,
+  icon: Icon, // 変更: JSXでのアイコン表示のため大文字で始める
   active,
+  label,
   onClick
 }) => {
   const handleClick = () => {
     if (onClick) {
-      return onClick();
+      onClick();
     }
   };
 
   return ( 
-    <Link 
-      onClick={handleClick} 
-      href={href} 
-      className={clsx(`
-        group 
-        flex 
-        gap-x-3 
-        text-sm 
-        leading-6 
-        font-semibold 
-        w-full 
-        justify-center 
-        p-4 
-        text-gray-500 
-        hover:text-black 
-        hover:bg-gray-100
-      `,
-        active && 'bg-gray-100 text-black',
-      )}>
-      <Icon className="h-6 w-6" />
+    <Link href={href}>
+      <div 
+        onClick={handleClick}
+        className={clsx(
+          "group flex flex-col items-center gap-x-3 text-sm font-semibold w-full justify-center p-2 text-gray-500 hover:text-black hover:bg-gray-100",
+          active && "bg-gray-100 text-black",
+        )}
+      >
+        <Icon className="h-6 w-6" />
+        <span className="text-xs">{label}</span> {/* ラベルを表示 */}
+      </div>
     </Link>
-   );
+  );
 }
- 
+
 export default MobileItem;
