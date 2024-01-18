2c68dc59a41e27e65f75139046ffa04b6b28870a
jiri0521 <47916493+jiri0521@users.noreply.github.com>
2024-01-18 21:39:24 +0900

    Update _document.js

--- pages/_document.js
+++ pages/_document.js
@@ -10,25 +10,6 @@ class MyDocument extends Document {
           <link rel="service-worker" href="/service-worker.js" />
           <link rel="apple-touch-icon" href="/icon-192x192.png"></link>
           <meta name="theme-color" content="#fff" />
-      
-<script defer src="https://www.pushcode.jp/dist/js/pushcode.js"></script>
-<script>
-window.PushCodeInit = function() {
-  try {
-    if (PushCode && PushCode.isSupport()) {
-      PushCode.init({ domainToken: 'a95d6a61cd54c5d393f520d66994a5d34edef682941984dec5cc3096a864f0f4', userid: '' });
-      PushCode.components.openSubscribeDialog();
-    }
-  }
-  catch (err) {
-    console.error(err);
-    if (PushCode) {
-      PushCode.sendError(err);
-    }
-  }
-};
-</script>
-
         </Head>
         <body>
           <Main />
