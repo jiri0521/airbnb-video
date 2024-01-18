9298d02c4f58a87352552c7296515c62891ab072
jiri0521 <47916493+jiri0521@users.noreply.github.com>
2024-01-18 22:02:02 +0900

    Update _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -17,8 +17,32 @@ function MyApp({ Component, pageProps }) {
       });
     }
   }, []);
-  
-  return <Component {...pageProps} />;
+ 
+  return(
+    <>
+            <script defer src="https://www.pushcode.jp/dist/js/pushcode.js"></script>
+        <script>
+        window.PushCodeInit = function() {
+          try {
+            if (PushCode && PushCode.isSupport()) {
+              PushCode.init({ domainToken: 'a95d6a61cd54c5d393f520d66994a5d34edef682941984dec5cc3096a864f0f4', userid: '' });
+              PushCode.components.openSubscribeDialog();
+            }
+          }
+          catch (err) {
+            console.error(err);
+            if (PushCode) {
+              PushCode.sendError(err);
+            }
+          }
+        };
+        // スクリプトが読み込まれた後、window.PushCodeInit()関数を実行します
+          window.PushCodeInit();
+        </script>
+          
+      <Component {...pageProps} />;
+  </>
+  );
 }
 
 export default MyApp;
