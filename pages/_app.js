c5dfc3d3a07a5303992cd12500022237ea57e69a
jiri0521 <47916493+jiri0521@users.noreply.github.com>
2024-01-18 22:03:24 +0900

    Update _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -18,27 +18,34 @@ function MyApp({ Component, pageProps }) {
     }
   }, []);
  
-  return(
+ return (
     <>
-            <script defer src="https://www.pushcode.jp/dist/js/pushcode.js"></script>
-        <script>
-        window.PushCodeInit = function() {
-          try {
-            if (PushCode && PushCode.isSupport()) {
-              PushCode.init({ domainToken: 'a95d6a61cd54c5d393f520d66994a5d34edef682941984dec5cc3096a864f0f4', userid: '' });
-              PushCode.components.openSubscribeDialog();
+      {/* Next.jsのScriptコンポーネントを使用して外部スクリプトを追加 */}
+      <Script
+        src="https://www.pushcode.jp/dist/js/pushcode.js"
+        strategy="afterInteractive"
+        onLoad={() => {
+          window.PushCodeInit = function() {
+            try {
+              if (PushCode && PushCode.isSupport()) {
+                PushCode.init({
+                  domainToken: 'a95d6a61cd54c5d393f520d66994a5d34edef682941984dec5cc3096a864f0f4',
+                  userid: '' // ここに適切なユーザーIDをセットしてください
+                });
+                PushCode.components.openSubscribeDialog();
+              }
+            } catch (err) {
+              console.error(err);
+              if (PushCode) {
+                PushCode.sendError(err);
+              }
             }
-          }
-          catch (err) {
-            console.error(err);
-            if (PushCode) {
-              PushCode.sendError(err);
-            }
-          }
-        };
-        // スクリプトが読み込まれた後、window.PushCodeInit()関数を実行します
+          };
+          // スクリプトが読み込まれた後、window.PushCodeInit()関数を実行します
           window.PushCodeInit();
-        </script>
+        }}
+      />
+      {/* この下にページの主要なコンポーネントが表示されます */}
           
       <Component {...pageProps} />;
   </>
