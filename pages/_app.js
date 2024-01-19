170c35ee9effba0b1f9dc192634dcfed01964237
jiri0521 <47916493+jiri0521@users.noreply.github.com>
2024-01-19 16:26:27 +0900

    Update _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -22,34 +22,19 @@ function MyApp({ Component, pageProps }) {
  
  return (
     <>
-      {/* Next.jsのScriptコンポーネントを使用して外部スクリプトを追加 */}
-      <Script
-        src="https://www.pushcode.jp/dist/js/pushcode.js"
-        strategy="afterInteractive"
-        onLoad={() => {
-          window.PushCodeInit = function() {
-            try {
-              if (PushCode && PushCode.isSupport()) {
-                PushCode.init({
-                  domainToken: 'a95d6a61cd54c5d393f520d66994a5d34edef682941984dec5cc3096a864f0f4',
-                  userid: '' // ここに適切なユーザーIDをセットしてください
-                });
-                PushCode.components.openSubscribeDialog();
-              }
-            } catch (err) {
-              console.error(err);
-              if (PushCode) {
-                PushCode.sendError(err);
-              }
-            }
-          };
-          // スクリプトが読み込まれた後、window.PushCodeInit()関数を実行します
-          window.PushCodeInit();
-        }}
-      />
+                {/* Next.jsのScriptコンポーネントを使用して外部スクリプトを追加 */}
+                <script src="https://cdn.onesignal.com/sdks/web/v16/OneSignalSDK.page.js" defer></script>
+          <script>
+            window.OneSignalDeferred = window.OneSignalDeferred || [];
+            OneSignalDeferred.push(function(OneSignal) {
+              OneSignal.init({
+                appId: "584d9f8f-3fb4-4843-ad96-5c400641f934",
+              })};
+            );
+          </script>
       {/* この下にページの主要なコンポーネントが表示されます */}
-          
-      <Component {...pageProps} />;
+         <Component {...pageProps} />
+  
   </>
   );
 }
