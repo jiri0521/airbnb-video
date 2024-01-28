5ff22146b2af47d53de4b147a2797615c1243775
jiri0521 <47916493+jiri0521@users.noreply.github.com>
2024-01-28 11:50:17 +0900

    Update UserMenu.tsx

--- app/components/navbar/UserMenu.tsx
+++ app/components/navbar/UserMenu.tsx
@@ -77,18 +77,18 @@ const UserMenu: React.FC<UserMenuProps> = ({
     <div className="relative">
         <div className="flex flex-row items-center gap-3">
           <div
-                onClick={onRent}
-                className="
+               className="
                 hidden
-               
+               md:block
                 text-sm
                 font-semibold
                 py-3
                 px-4
                 rounded-full
-                hover:bg-neutral-100
+              
                 transition
-                cursor-pointer
+                
+                "
                 "
                 >
 
