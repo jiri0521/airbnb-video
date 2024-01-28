85c701b5a2a044a1c69465fad85db06df035d2cc
jiri0521 <47916493+jiri0521@users.noreply.github.com>
2024-01-28 11:20:35 +0900

    Update UserMenu.tsx

--- app/components/navbar/UserMenu.tsx
+++ app/components/navbar/UserMenu.tsx
@@ -76,7 +76,21 @@ const UserMenu: React.FC<UserMenuProps> = ({
     return( 
     <div className="relative">
         <div className="flex flex-row items-center gap-3">
-           
+           <div
+                
+                className="
+                hidden
+                md:block
+                text-sm
+                font-semibold
+                py-3
+                px-4
+                rounded-full
+                
+                transition
+               
+                "
+                >
             <div 
                 onClick={toggleOpen}
                 className="
