d85035de48cc52b69768f5584a34913561494de5
jiri0521 <47916493+jiri0521@users.noreply.github.com>
2024-02-07 14:59:54 +0900

    Update layout.tsx

--- app/kitchen/layout.tsx
+++ app/kitchen/layout.tsx
@@ -1,4 +1,4 @@
-// bedroom/layout.tsx
+// kitchen/layout.tsx
 import Link from 'next/link';
 import Image from 'next/image';
 import layout from 'public/images/kitchen3.webp';
@@ -18,14 +18,14 @@ const LayoutPlan = () => {
       <div className="absolute top-0 left-0 w-full h-full">
        
         {/* div要素に 'balloon' スタイルクラスとアニメーションを適用します */}
-        <Link href="/?category=栄養">
+        <Link href="https://air-video.vercel.app/listings/659743d7ecfec5c8548a5770">
         <div className='w-full h-full animate-wiggle text-sm text-white bg-red-400 flex items-center justify-center' style={{ position: 'absolute', top: '55%', left: '28%', width: '15%', height: '15%' }}>とろみの付け方</div>
         </Link>
-        {/* 寝室へのリンク */}
+        {/* リンク */}
         <Link href="https://air-video.vercel.app/listings/6531e81803f3d1b1705b8d45">
         <div className='w-full h-full animate-wiggle text-sm text-white bg-red-400 flex items-center justify-center' style={{ position: 'absolute', top: '28%', left: '58%', width: '15%', height: '15%' }}>食べさせ方</div>
         </Link>
-        <Link href="/?category=歩行">
+        <Link href="/?category=栄養">
         <div className='w-full h-full animate-wiggle text-sm text-white bg-red-400 flex items-center justify-center' style={{ position: 'absolute', top: '55%', left: '58%', width: '15%', height: '15%' }}>乳製品のとろみの付け方</div>
         </Link>
       </div>
@@ -39,3 +39,4 @@ const LayoutPlan = () => {
 };
 
 export default LayoutPlan;
+
