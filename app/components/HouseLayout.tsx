cfd84644a9b655b9eb41fe8c138a46424c9dbeba
jiri0521 <47916493+jiri0521@users.noreply.github.com>
2024-02-08 23:20:33 +0900

    Update HouseLayout.tsx

--- app/components/HouseLayout.tsx
+++ app/components/HouseLayout.tsx
@@ -1,10 +1,11 @@
 // components/LayoutPlan.tsx
 import Link from 'next/link';
 import Image from 'next/image';
-import layout from 'public/images/layout.svg';
+import layout from 'public/images/layout3.png';
 import balloonBed from 'public/images/balloon-bed.svg'; // インポート確認済み
 import balloonKitchen from 'public/images/balloon-kitchen.svg';
 import balloonLiving from 'public/images/balloon-living.svg';
+
 
 const LayoutPlan = () => {
   
@@ -15,23 +16,22 @@ const LayoutPlan = () => {
       <Image src={layout} alt="3LDK Layout" width={700} height={700} />
       <div className="absolute top-0 left-0 w-full h-full">
         {/* div要素に 'balloon' スタイルクラスとアニメーションを適用します */}
-         <Link href="/kitchen">
-        <Image src={balloonKitchen} className='w-full h-full animate-wiggle' alt="bollon-eiyou" style={{ position: 'absolute', top: '55%', left: '28%', width: '15%', height: '15%' }}/>
+        <Link href="/kitchen">
+        <Image src={balloonKitchen} className='w-full h-full animate-wiggle' alt="bollon-eiyou" style={{ position: 'absolute', top: '28%', left: '55%', width: '15%', height: '15%' }}/>
         </Link>
         {/* 寝室へのリンク */}
         <Link href="/bedroom">
-        <Image src={balloonBed} className='w-full h-full animate-wiggle' alt="bollon-bed" style={{ position: 'absolute', top: '28%', left: '58%', width: '15%', height: '15%' }}/>
+        <Image src={balloonBed} className='w-full h-full animate-wiggle' alt="bollon-bed" style={{ position: 'absolute', top: '55%', left: '35%', width: '15%', height: '15%' }}/>
         </Link>
         <Link href="/living">
-        <Image src={balloonLiving} className='w-full h-full animate-wiggle' alt="bollon-living" style={{ position: 'absolute', top: '55%', left: '58%', width: '15%', height: '15%' }}/>
+        <Image src={balloonLiving} className='w-full h-full animate-wiggle' alt="bollon-living" style={{ position: 'absolute', top: '55%', left: '65%', width: '15%', height: '15%' }}/>
         </Link>
       </div>
      
-      <a className='text-gray-400'>
-        イラストの著作者:macrovector / 出典:Freepik
-      </a>
+      
     </div>
   );
 };
 
 export default LayoutPlan;
+
