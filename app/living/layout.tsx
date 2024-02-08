409dc316462e3a7a5af52b37ea3db974e296b194
jiri0521 <47916493+jiri0521@users.noreply.github.com>
2024-02-08 23:21:31 +0900

    Update layout.tsx

--- app/living/layout.tsx
+++ app/living/layout.tsx
@@ -1,41 +1,105 @@
 // living/layout.tsx
+
+'use client';
+
 import Link from 'next/link';
 import Image from 'next/image';
 import layout from 'public/images/living.webp';
-import balloonBed from 'public/images/balloon-bed.svg'; // インポート確認済み
-import balloonKitchen from 'public/images/balloon-kitchen.svg';
-import balloonLiving from 'public/images/balloon-living.svg';
+import DeepSitDownCat from 'public/images/ねこ深く座る.png';
+import StandUpCat from 'public/images/ねこ床からの立ち上がり.png';
 import FloatingHomeButton from '../components/FloatingHomeButton';
+import React, { useState } from 'react';
+
+interface ModalContent {
+  title: string;
+  description: string;
+  videoUrl: string;
+}
+
 
 const LayoutPlan = () => {
   
+  const [isModalOpen, setIsModalOpen] = useState(false);
+  const [modalContent, setModalContent] = useState<ModalContent>({ title: '', description: '',videoUrl:''});
+
+  const openModal = (content: ModalContent) => {
+    setModalContent(content);
+    setIsModalOpen(true);
+  };
+
+  const closeModal = () => {
+    setIsModalOpen(false);
+  };
 
   return (
-    
+    <>
+    <style jsx global>{`
+      @keyframes slideInUp {
+        from {
+          transform: translateY(100%);
+        }
+        to {
+          transform: translateY(0);
+        }
+      }
+    `}</style>
     <div className="flex flex-col relative items-center">
       <Image src={layout} alt="bedroom Layout" width={700} height={700} /> 
+      
       <FloatingHomeButton /> 
       <div className="absolute top-0 left-0 w-full h-full">
        
         {/* div要素に 'balloon' スタイルクラスとアニメーションを適用します */}
-        <Link href="https://air-video.vercel.app/listings/656983eac7fbc06fa83d52ca">
-        <div className='w-full h-full animate-wiggle text-sm text-orange-400 bg-white flex items-center justify-center' style={{ position: 'absolute', top: '55%', left: '28%', width: '15%', height: '15%' }}>深く座る</div>
-        </Link>
+       
         {/* 寝室へのリンク */}
-        <Link href="https://air-video.vercel.app/listings/6531e81803f3d1b1705b8d45">
-        <div className='w-full h-full animate-wiggle text-sm text-orange-400 bg-white flex items-center justify-center' style={{ position: 'absolute', top: '28%', left: '58%', width: '15%', height: '15%' }}>歩行介助</div>
-        </Link>
-        <Link href="https://air-video.vercel.app/listings/65530c41b735a967abc092de">
-        <div className='w-full h-full animate-wiggle text-sm text-orange-400 bg-white flex items-center justify-center' style={{ position: 'absolute', top: '55%', left: '58%', width: '15%', height: '15%' }}>床からの立ち上がり</div>
-        </Link>
-      </div>
-     
+       
+        <div className="cursor-pointer" onClick={() => openModal({ title: '深く座る', description: '作業療法士のウッチーが深く座り直す方法を解説・実演します', videoUrl: 'https://air-video.vercel.app/listings/656983eac7fbc06fa83d52ca' })}> 
+               
+               <Image src={DeepSitDownCat} alt="DeepSitDownCatCat" width={100} height={100} className='animate-wiggle' style={{ position: 'absolute', top: '40%', left: '28%', width: '15%', height: '15%' }} />                
+       
+       </div>
+        </div>
+        
+        <div className="cursor-pointer" onClick={() => openModal({ title: '床からの立ち上がり', description: '作業療法士のウッチーが最小限の力で立ち上がらせる方法を解説・実演します', videoUrl: 'https://air-video.vercel.app/listings/65530c41b735a967abc092de' })}>               
+               
+                <Image src={StandUpCat} alt="StandUpCat" width={100} height={100} className='animate-wiggle' style={{ position: 'absolute', top: '55%', left: '58%', width: '15%', height: '15%' }} />            
+        
+        </div>
       <div className='text-gray-400'>
       © pixander 123RF Free Images
       </div>
-     
-    </div>
-  );
+
+      {isModalOpen && modalContent && (
+        <div className="fixed inset-0 bg-black bg-opacity-50 flex justify-center items-end p-4 z-50 " onClick={closeModal}>
+          <div
+              className="bg-white p-6 rounded-t-lg shadow-lg w-full max-w-md m-4 animate-slideInUp"
+              onClick={(e) => e.stopPropagation()}
+              style={{
+                animation: 'slideInUp 0.5s ease-out forwards'
+              }}
+            >
+            <h2 className="text-lg font-bold mb-2">{modalContent.title}</h2>
+            <p>{modalContent.description}</p>
+            <a
+              href={modalContent.videoUrl}
+              target="_blank"
+              rel="noopener noreferrer"
+              className="mt-4 bg-blue-500 hover:bg-blue-700 text-white py-2 px-4 rounded focus:outline-none focus:shadow-outline"
+            >
+              動画視聴
+            </a>
+            <button
+              className="mt-4 bg-gray-500 hover:bg-gray-700 text-white py-2 px-4 rounded focus:outline-none focus:shadow-outline ml-2"
+              onClick={closeModal}
+            >
+              閉じる
+            </button>
+          </div>
+        </div>
+      )}
+       
+     </div>
+  </>);
 };
 
 export default LayoutPlan;
