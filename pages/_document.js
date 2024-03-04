43fc105f92703fb5feb9a1ab8253570f8f0ce315
jiri0521 <47916493+jiri0521@users.noreply.github.com>
2024-03-04 12:01:06 +0900

    Update _document.js

--- pages/_document.js
+++ pages/_document.js
@@ -10,15 +10,7 @@ class MyDocument extends Document {
           <link rel="service-worker" href="/service-worker.js" />
           <link rel="apple-touch-icon" href="/icon-192x192.png"></link>
           <meta name="theme-color" content="#fff" />
-     
-<script async src="https://www.googletagmanager.com/gtag/js?id=G-HG1DMWYQ2R"></script>
-<script>
-  window.dataLayer = window.dataLayer || [];
-  function gtag(){dataLayer.push(arguments);}
-  gtag('js', new Date());
-
-  gtag('config', 'G-HG1DMWYQ2R');
-</script>
+    
         </Head>
         <body>
           <Main />
