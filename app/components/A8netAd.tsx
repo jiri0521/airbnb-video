7e8025f6d8493b4a61d2219eb79252c7528c06f2
jiri0521 <47916493+jiri0521@users.noreply.github.com>
2024-04-19 23:27:55 +0900

    Update A8netAd.tsx

--- app/components/A8netAd.tsx
+++ app/components/A8netAd.tsx
@@ -9,7 +9,7 @@ const A8NetAd: React.FC = () => {
     <div className='flex justify-center items-center '>
  
       <Link href="https://px.a8.net/svt/ejp?a8mat=3Z4U5E+G23X6A+5HQC+BXB8X" rel="nofollow noopener noreferrer" target="_blank" className="block w-300 h-250">
-      【広告】腰の悩み "ぎゅ〜っ"で解決！
+      【広告】腰の悩み ぎゅ〜っで解決！
           <Image
           className='rounded-xl'
           src="https://www25.a8.net/svt/bgt?aid=240410786971&wid=001&eno=01&mid=s00000025626002003000&mc=1"
