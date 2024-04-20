940ac29fca3755ec5eb25b77b163fc7b89a015be
jiri0521 <47916493+jiri0521@users.noreply.github.com>
2024-04-20 09:47:17 +0900

    Update A8netAd.tsx

--- app/components/A8netAd.tsx
+++ app/components/A8netAd.tsx
@@ -1,33 +1,87 @@
 import React from 'react';
 import Link from 'next/link';
 import Image from 'next/image';
-import error from 'public/images/error.png';
 
 
 const A8NetAd: React.FC = () => {
   return (
-    <div className='flex justify-center items-center '>
- 
-      <Link href="https://px.a8.net/svt/ejp?a8mat=3Z4U5E+G23X6A+5HQC+BXB8X" rel="nofollow noopener noreferrer" target="_blank" className="block w-300 h-250">
-      【広告】腰の悩み ぎゅ〜っで解決！
+    <div className="flex justify-center items-center">
+    <div className="flex flex-col gap-4">
+      <div className="flex justify-center">
+        <Link
+          href="https://px.a8.net/svt/ejp?a8mat=3Z4U5E+G23X6A+5HQC+BXB8X"
+          rel="nofollow noopener noreferrer"
+          target="_blank"
+          className="block w-300 h-250"
+        >
+          【広告】腰の悩み ぎゅ〜っで解決！
           <Image
-          className='rounded-xl'
-          src="https://www25.a8.net/svt/bgt?aid=240410786971&wid=001&eno=01&mid=s00000025626002003000&mc=1"
-          alt=""
-          width={300}
-          height={250}
+            className="rounded-xl cursor-pointer"
+            src="https://www25.a8.net/svt/bgt?aid=240410786971&wid=001&eno=01&mid=s00000025626002003000&mc=1"
+            alt=""
+            width={300}
+            height={250}
           />
-           <Image
-          className=''
-          src="https://www13.a8.net/0.gif?a8mat=3Z4U5E+G23X6A+5HQC+BXQOH"
-          alt=""
-          width={1}
-          height={1}
-        />
-      </Link>
+          <Image
+            className=""
+            src="https://www13.a8.net/0.gif?a8mat=3Z4U5E+G23X6A+5HQC+BXQOH"
+            alt=""
+            width={1}
+            height={1}
+          />
+        </Link>
+      </div>
+      <div className="bg-white rounded-lg mx-0 py-5 px-5 text-center overflow-hidden">
+        <div className="grid grid-cols-2 gap-4">
+          <div>
+            <Link
+              href="https://hb.afl.rakuten.co.jp/ichiba/3b80fe65.f2663ce2.3b80fe66.29ff1d55/?pc=https%3A%2F%2Fitem.rakuten.co.jp%2Ffine-kagaku%2Ftrmnl-800g%2F&link_type=picttext&ut=eyJwYWdlIjoiaXRlbSIsInR5cGUiOiJwaWN0dGV4dCIsInNpemUiOiIyNDB4MjQwIiwibmFtIjoxLCJuYW1wIjoicmlnaHQiLCJjb20iOjEsImNvbXAiOiJkb3duIiwicHJpY2UiOjEsImJvciI6MSwiY29sIjoxLCJiYnRuIjoxLCJwcm9kIjowLCJhbXAiOmZhbHNlfQ%3D%3D"
+              target="_blank"
+              rel="nofollow sponsored noopener"
+            >
+              <Image
+                src="https://hbb.afl.rakuten.co.jp/hgb/3b80fe65.f2663ce2.3b80fe66.29ff1d55/?me_id=1285720&item_id=10000135&pc=https%3A%2F%2Fthumbnail.image.rakuten.co.jp%2F%400_mall%2Ffine-kagaku%2Fcabinet%2Fimages%2Fmain%2Fa-200003.jpg%3F_ex%3D240x240&s=240x240&t=picttext"
+                alt="[商品価格に関しましては、リンクが作成された時点と現時点で情報が変更されている場合がございます。]"
+                title="[商品価格に関しましては、リンクが作成された時点と現時点で情報が変更されている場合がございます。]"
+                className="mx-2"
+                width={300}
+                height={250}
+              />
+            </Link>
+          </div>
+          <div className="flex flex-col justify-center">
+            <p className="text-sm leading-6 text-left mb-2">
+              <a
+                href="https://hb.afl.rakuten.co.jp/ichiba/3b80fe65.f2663ce2.3b80fe66.29ff1d55/?pc=https%3A%2F%2Fitem.rakuten.co.jp%2Ffine-kagaku%2Ftrmnl-800g%2F&link_type=picttext&ut=eyJwYWdlIjoiaXRlbSIsInR5cGUiOiJwaWN0dGV4dCIsInNpemUiOiIyNDB4MjQwIiwibmFtIjoxLCJuYW1wIjoicmlnaHQiLCJjb20iOjEsImNvbXAiOiJkb3duIiwicHJpY2UiOjEsImJvciI6MSwiY29sIjoxLCJiYnRuIjoxLCJwcm9kIjowLCJhbXAiOmZhbHNlfQ%3D%3D"
+                target="_blank"
+                rel="nofollow sponsored noopener"
+              >
+                【20日P5倍】トロミ ナール 800g トロミ 剤 とろみ 剤 嚥下 能力 誤嚥 緩和 医療 食 介護 食 ファイン
+              </a>
+            </p>
+            <p className="text-sm text-gray-500">(2024/4/20時点)</p>
+            <div className="flex justify-between items-center mt-4">
+              <Link
+                href="https://hb.afl.rakuten.co.jp/ichiba/3b80fe65.f2663ce2.3b80fe66.29ff1d55/?pc=https%3A%2F%2Fitem.rakuten.co.jp%2Ffine-kagaku%2Ftrmnl-800g%2F&link_type=picttext&ut=eyJwYWdlIjoiaXRlbSIsInR5cGUiOiJwaWN0dGV4dCIsInNpemUiOiIyNDB4MjQwIiwibmFtIjoxLCJuYW1wIjoicmlnaHQiLCJjb20iOjEsImNvbXAiOiJkb3duIiwicHJpY2UiOjEsImJvciI6MSwiY29sIjoxLCJiYnRuIjoxLCJwcm9kIjowLCJhbXAiOmZhbHNlfQ%3D%3D"
+                target="_blank"
+                rel="nofollow sponsored noopener"
+              >
+                <Image
+                  src="https://static.affiliate.rakuten.co.jp/makelink/rl.svg"
+                  className="max-h-[27px] w-auto"
+                  alt=""
+                  width={300}
+                  height={250}
+                />
+              </Link>
+        <a href="https://hb.afl.rakuten.co.jp/ichiba/3b80fe65.f2663ce2.3b80fe66.29ff1d55/?pc=https%3A%2F%2Fitem.rakuten.co.jp%2Ffine-kagaku%2Ftrmnl-800g%2F%3Fscid%3Daf_pc_bbtn&link_type=picttext&ut=eyJwYWdlIjoiaXRlbSIsInR5cGUiOiJwaWN0dGV4dCIsInNpemUiOiIyNDB4MjQwIiwibmFtIjoxLCJuYW1wIjoicmlnaHQiLCJjb20iOjEsImNvbXAiOiJkb3duIiwicHJpY2UiOjEsImJvciI6MSwiY29sIjoxLCJiYnRuIjoxLCJwcm9kIjowLCJhbXAiOmZhbHNlfQ==" target="_blank" rel="nofollow sponsored noopener" className="bg-[#bf0000] text-white font-medium text-sm leading-[27px] px-3 py-0 rounded-full cursor-pointer">楽天で購入</a>
+      </div>
+    </div>
+  </div>
+</div>
+</div>
     </div>
   );
 };
 
 export default A8NetAd;
-
