e884ed5e6f1bc88bd481b8119111727c115fd83f
jiri0521 <47916493+jiri0521@users.noreply.github.com>
2024-04-20 11:28:58 +0900

    Update A8netAd.tsx

--- app/components/A8netAd.tsx
+++ app/components/A8netAd.tsx
@@ -3,6 +3,7 @@ import Link from 'next/link';
 import Image from 'next/image';
 
 
+
 const A8NetAd: React.FC = () => {
   return (
     <div className="flex justify-center items-center">
@@ -35,46 +36,51 @@ const A8NetAd: React.FC = () => {
         <div className="grid grid-cols-2 gap-4">
           <div>
             <Link
-              href="https://hb.afl.rakuten.co.jp/ichiba/3b80fe65.f2663ce2.3b80fe66.29ff1d55/?pc=https%3A%2F%2Fitem.rakuten.co.jp%2Ffine-kagaku%2Ftrmnl-800g%2F&link_type=picttext&ut=eyJwYWdlIjoiaXRlbSIsInR5cGUiOiJwaWN0dGV4dCIsInNpemUiOiIyNDB4MjQwIiwibmFtIjoxLCJuYW1wIjoicmlnaHQiLCJjb20iOjEsImNvbXAiOiJkb3duIiwicHJpY2UiOjEsImJvciI6MSwiY29sIjoxLCJiYnRuIjoxLCJwcm9kIjowLCJhbXAiOmZhbHNlfQ%3D%3D"
-              target="_blank"
-              rel="nofollow sponsored noopener"
+             href="https://hb.afl.rakuten.co.jp/ichiba/3b819cd7.3175ee17.3b819cd8.09bf5d27/?pc=https%3A%2F%2Fitem.rakuten.co.jp%2Fladydrugplus%2F4902705024731%2F&link_type=picttext&ut=eyJwYWdlIjoiaXRlbSIsInR5cGUiOiJwaWN0dGV4dCIsInNpemUiOiIxMjh4MTI4IiwibmFtIjoxLCJuYW1wIjoicmlnaHQiLCJjb20iOjEsImNvbXAiOiJkb3duIiwicHJpY2UiOjEsImJvciI6MSwiY29sIjoxLCJiYnRuIjoxLCJwcm9kIjowLCJhbXAiOmZhbHNlfQ%3D%3D"
+             target="_blank"
+             rel="nofollow sponsored noopener"
+             className="block"
             >
               <Image
-                src="https://hbb.afl.rakuten.co.jp/hgb/3b80fe65.f2663ce2.3b80fe66.29ff1d55/?me_id=1285720&item_id=10000135&pc=https%3A%2F%2Fthumbnail.image.rakuten.co.jp%2F%400_mall%2Ffine-kagaku%2Fcabinet%2Fimages%2Fmain%2Fa-200003.jpg%3F_ex%3D240x240&s=240x240&t=picttext"
+                src="https://hbb.afl.rakuten.co.jp/hgb/3b819cd7.3175ee17.3b819cd8.09bf5d27/?me_id=1359324&item_id=10104132&pc=https%3A%2F%2Fthumbnail.image.rakuten.co.jp%2F%400_mall%2Fladydrugplus%2Fcabinet%2Fgazo115%2F4902705024731_2.jpg%3F_ex%3D240x240&s=240x240&t=picttext"
                 alt="[商品価格に関しましては、リンクが作成された時点と現時点で情報が変更されている場合がございます。]"
                 title="[商品価格に関しましては、リンクが作成された時点と現時点で情報が変更されている場合がございます。]"
-                className="mx-2"
-                width={300}
-                height={250}
+                className="rounded-lg"              
+                width={240}
+                height={240}
               />
             </Link>
           </div>
           <div className="flex flex-col justify-center">
             <p className="text-sm leading-6 text-left mb-2">
-              <a
-                href="https://hb.afl.rakuten.co.jp/ichiba/3b80fe65.f2663ce2.3b80fe66.29ff1d55/?pc=https%3A%2F%2Fitem.rakuten.co.jp%2Ffine-kagaku%2Ftrmnl-800g%2F&link_type=picttext&ut=eyJwYWdlIjoiaXRlbSIsInR5cGUiOiJwaWN0dGV4dCIsInNpemUiOiIyNDB4MjQwIiwibmFtIjoxLCJuYW1wIjoicmlnaHQiLCJjb20iOjEsImNvbXAiOiJkb3duIiwicHJpY2UiOjEsImJvciI6MSwiY29sIjoxLCJiYnRuIjoxLCJwcm9kIjowLCJhbXAiOmZhbHNlfQ%3D%3D"
-                target="_blank"
-                rel="nofollow sponsored noopener"
-              >
-                【20日P5倍】トロミ ナール 800g トロミ 剤 とろみ 剤 嚥下 能力 誤嚥 緩和 医療 食 介護 食 ファイン
-              </a>
-            </p>
-            <p className="text-sm text-gray-500">(2024/4/20時点)</p>
+            <a
+          href="https://hb.afl.rakuten.co.jp/ichiba/3b819cd7.3175ee17.3b819cd8.09bf5d27/?pc=https%3A%2F%2Fitem.rakuten.co.jp%2Fladydrugplus%2F4902705024731%2F&link_type=picttext&ut=eyJwYWdlIjoiaXRlbSIsInR5cGUiOiJwaWN0dGV4dCIsInNpemUiOiIxMjh4MTI4IiwibmFtIjoxLCJuYW1wIjoicmlnaHQiLCJjb20iOjEsImNvbXAiOiJkb3duIiwicHJpY2UiOjEsImJvciI6MSwiY29sIjoxLCJiYnRuIjoxLCJwcm9kIjowLCJhbXAiOmZhbHNlfQ%3D%3D"
+          target="_blank"
+          rel="nofollow sponsored noopener"
+        >
+          明治 かんたん トロメイク 400g
+        </a>
+      </p>
+      <p className="text-sm text-gray-500">(2024/4/20時点)</p>
+      <p className="text-sm leading-6 text-left mt-4">
+        価格:2,327円 (税込、送料無料)
+      </p>
             <div className="flex justify-between items-center mt-4">
               <Link
-                href="https://hb.afl.rakuten.co.jp/ichiba/3b80fe65.f2663ce2.3b80fe66.29ff1d55/?pc=https%3A%2F%2Fitem.rakuten.co.jp%2Ffine-kagaku%2Ftrmnl-800g%2F&link_type=picttext&ut=eyJwYWdlIjoiaXRlbSIsInR5cGUiOiJwaWN0dGV4dCIsInNpemUiOiIyNDB4MjQwIiwibmFtIjoxLCJuYW1wIjoicmlnaHQiLCJjb20iOjEsImNvbXAiOiJkb3duIiwicHJpY2UiOjEsImJvciI6MSwiY29sIjoxLCJiYnRuIjoxLCJwcm9kIjowLCJhbXAiOmZhbHNlfQ%3D%3D"
+                   href="https://hb.afl.rakuten.co.jp/ichiba/3b819cd7.3175ee17.3b819cd8.09bf5d27/?pc=https%3A%2F%2Fitem.rakuten.co.jp%2Fladydrugplus%2F4902705024731%2F&link_type=picttext&ut=eyJwYWdlIjoiaXRlbSIsInR5cGUiOiJwaWN0dGV4dCIsInNpemUiOiIyNDB4MjQwIiwibmFtIjoxLCJuYW1wIjoicmlnaHQiLCJjb20iOjEsImNvbXAiOiJkb3duIiwicHJpY2UiOjEsImJvciI6MSwiY29sIjoxLCJiYnRuIjoxLCJwcm9kIjowLCJhbXAiOmZhbHNlfQ%3D%3D"
                 target="_blank"
                 rel="nofollow sponsored noopener"
+                className="inline-flex items-center"
               >
                 <Image
                   src="https://static.affiliate.rakuten.co.jp/makelink/rl.svg"
                   className="max-h-[27px] w-auto"
                   alt=""
-                  width={300}
-                  height={250}
+                  width={240}
+                  height={240}
                 />
               </Link>
-        <a href="https://hb.afl.rakuten.co.jp/ichiba/3b80fe65.f2663ce2.3b80fe66.29ff1d55/?pc=https%3A%2F%2Fitem.rakuten.co.jp%2Ffine-kagaku%2Ftrmnl-800g%2F%3Fscid%3Daf_pc_bbtn&link_type=picttext&ut=eyJwYWdlIjoiaXRlbSIsInR5cGUiOiJwaWN0dGV4dCIsInNpemUiOiIyNDB4MjQwIiwibmFtIjoxLCJuYW1wIjoicmlnaHQiLCJjb20iOjEsImNvbXAiOiJkb3duIiwicHJpY2UiOjEsImJvciI6MSwiY29sIjoxLCJiYnRuIjoxLCJwcm9kIjowLCJhbXAiOmZhbHNlfQ==" target="_blank" rel="nofollow sponsored noopener" className="bg-[#bf0000] text-white font-medium text-sm leading-[27px] px-3 py-0 rounded-full cursor-pointer">楽天で購入</a>
+        <a href="https://hb.afl.rakuten.co.jp/ichiba/3b819cd7.3175ee17.3b819cd8.09bf5d27/?pc=https%3A%2F%2Fitem.rakuten.co.jp%2Fladydrugplus%2F4902705024731%2F%3Fscid%3Daf_pc_bbtn&link_type=picttext&ut=eyJwYWdlIjoiaXRlbSIsInR5cGUiOiJwaWN0dGV4dCIsInNpemUiOiIyNDB4MjQwIiwibmFtIjoxLCJuYW1wIjoicmlnaHQiLCJjb20iOjEsImNvbXAiOiJkb3duIiwicHJpY2UiOjEsImJvciI6MSwiY29sIjoxLCJiYnRuIjoxLCJwcm9kIjowLCJhbXAiOmZhbHNlfQ==" target="_blank" rel="nofollow sponsored noopener" className="bg-[#bf0000] text-white font-medium text-sm leading-[27px] px-3 py-0 rounded-full cursor-pointer">楽天で購入</a>
       </div>
     </div>
   </div>
