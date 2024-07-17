b5f2fd4354a5adade50d397f1c28d2f7c2bb7dcf
jiri0521 <47916493+jiri0521@users.noreply.github.com>
2024-07-17 23:01:40 +0900

    Update HouseLayout.tsx

--- app/components/HouseLayout.tsx
+++ app/components/HouseLayout.tsx
@@ -12,26 +12,24 @@ const LayoutPlan = () => {
 
   return (
     <div className="flex flex-col relative items-center">
-     
-       <Image src={layout} alt="House Layout" width={700} height={700} />
-      <div className="absolute top-0 left-0 w-full h-full">
-        {/* div要素に 'balloon' スタイルクラスとアニメーションを適用します */}
-        <Link href="/kitchen">
-        <Image src={balloonKitchen} className='w-full h-full animate-wiggle' alt="bollon-eiyou" style={{ position: 'absolute', top: '20%', left: '70%', width: '15%', height: '15%' }}/>
-        </Link>
-        {/* 寝室へのリンク */}
-        <Link href="/bedroom">
-        <Image src={balloonBed} className='w-full h-full animate-wiggle' alt="bollon-bed" style={{ position: 'absolute', top: '55%', left: '20%', width: '15%', height: '15%' }}/>
-        </Link>
-        <Link href="/living">
-        <Image src={balloonLiving} className='w-full h-full animate-wiggle' alt="bollon-living" style={{ position: 'absolute', top: '55%', left: '65%', width: '15%', height: '15%' }}/>
-        </Link>
+      <div className="w-[400px] h-[400px] relative">
+        <Image src={layout} alt="house Layout" width={400} height={400} />
+        <div className="absolute top-0 left-0 w-full h-full">
+          {/* div要素に 'balloon' スタイルクラスとアニメーションを適用します */}
+          <Link href="/kitchen">
+            <Image src={balloonKitchen} className='w-full h-full animate-wiggle' alt="bollon-eiyou" style={{ position: 'absolute', top: '25%', left: '70%', width: '15%', height: '15%' }}/>
+          </Link>
+          {/* 寝室へのリンク */}
+          <Link href="/bedroom">
+            <Image src={balloonBed} className='w-full h-full animate-wiggle' alt="bollon-bed" style={{ position: 'absolute', top: '60%', left: '20%', width: '15%', height: '15%' }}/>
+          </Link>
+          <Link href="/living">
+            <Image src={balloonLiving} className='w-full h-full animate-wiggle' alt="bollon-living" style={{ position: 'absolute', top: '60%', left: '65%', width: '15%', height: '15%' }}/>
+          </Link>
+        </div>
       </div>
-     
-      
     </div>
   );
 };
 
 export default LayoutPlan;
-
